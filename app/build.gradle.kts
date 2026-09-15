caeb290e0f92636dc327e3598068e6bf1da136da
J05ue21 <orellana3566@gmail.com>
2026-09-15 14:10:56 -0600

    configuracion : dependencia de Firebase Storage [AddDestinoActivity no la encontraba]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
     implementation(libs.androidx.credentials.play.services.auth)
     implementation(libs.firebase.auth)
     implementation(libs.firebase.database)
+    implementation(libs.firebase.storage)
     implementation(libs.googleid)
     implementation(libs.material)
     testImplementation(libs.junit)
